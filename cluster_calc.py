860b6f0af8c6a2b03c7de080540a3b4d47f12cdb
Tigran Apinyan <apinyantigran0@gmail.com>
2023-06-13 22:06:40 +0400

    .

--- cluster_calc.py
+++ cluster_calc.py
@@ -7,9 +7,9 @@ from Task import Task
 
 parser = argparse.ArgumentParser(description="cluster task prediction")
 
-parser.add_argument('--cluster', help='path to the cluster configuration JSON file')
-parser.add_argument('--tasks', help='path to the task configuration JSON file')
-parser.add_argument('--output', help='path to the output JSON file')
+parser.add_argument('--cluster', help = 'path to the cluster configuration JSON file')
+parser.add_argument('--tasks', help = 'path to the task configuration JSON file')
+parser.add_argument('--output', help = 'path to the output JSON file')
 
 args = parser.parse_args()
 clusterConfigP = args.cluster
@@ -42,7 +42,6 @@ def read_task_config(file_path):
         for task_data in config['tasks']:
             task = Task(**task_data)
             tasks.append(task)
-        print(tasks)
         return tasks
     except FileNotFoundError:
             print(f'Error: JSON file {file_path} not found')
